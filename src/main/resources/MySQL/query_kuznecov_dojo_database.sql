2b37d6d9d955cca83f336e83d61dd7a09733ee26
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-08-24 09:44:09 +0300

    [+] - join

--- src/main/resources/MySQL/query_kuznecov_dojo_database.sql
+++ src/main/resources/MySQL/query_kuznecov_dojo_database.sql
@@ -1,5 +1,9 @@
 select * from instructor;
 
+select first_name, second_name, age, phone 
+from instructor
+where age > 30;
+
 select * from certificate;
 
 select * from sport_club;
@@ -7,3 +11,27 @@ select * from sport_club;
 select * from instructor_sport_club;
 
 select * from instructor, certificate;
+
+
+-- INNER JOIN
+-- select certificate.number, certificate.date_pass, certificate.degree
+-- from instructor
+-- inner join certificate on instructor.instr_id = certificate.certif_id
+
+select *
+from certificate
+inner join instructor
+on instructor.instr_id = certificate.instr_id
+order by instructor.instr_id;
+
+select instructor.second_name, instructor.instr_id, certificate.date_pass, certificate.number, certificate.instr_id 
+from certificate
+left join instructor
+on certificate.instr_id = 1;
+
+-- update table_name
+-- set column1 = value1, column2 = value2, ..., columnN = valueN
+-- where [condition];
+
+-- delete from table_name
+-- where [condition]; 
