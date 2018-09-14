b25b4afbfe4aadee25229d449869fe66f5f50e1a
AnastasiyaPesto <pestovnikova2013@gmail.com>
2018-09-14 13:57:34 +0300

    [*] Поиск инструктора по фамилии

--- src/main/java/web/InstructorsFindByFirstNameController.java
+++ src/main/java/web/InstructorsFindByFirstNameController.java
@@ -12,26 +12,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @Controller
-public class InstructorsFindByFirstName {
+public class InstructorsFindByFirstNameController {
     private final InstructorDAO instructorDAO;
 
     @Autowired
-    public InstructorsFindByFirstName(InstructorDAO instructorDAO) {
+    public InstructorsFindByFirstNameController(InstructorDAO instructorDAO) {
         this.instructorDAO = instructorDAO;
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/instructors/findByFirstName")
     public String findByFirstNamePostForm(@RequestParam String firstName, ModelMap modelMap) {
         List<Instructor> instructors = instructorDAO.findByFirstName(firstName);
-//        InstructorListBean instructorListBean = new InstructorListBean(instructors);
+        InstructorListBean instructorListBean = new InstructorListBean(instructors);
 
-//        modelMap.put("instructorListBean", instructorListBean);
+        modelMap.put("instructorListBean", instructorListBean);
 
-        return "instructor-findbyfirstname";
+        return "instructor-foundedinstructorsbyfirstname";
     }
 
+//    @RequestMapping(method = RequestMethod.GET, path = "/instructors/foundedByFirstName")
+//    public String foundedInstructors() {
+//        return "instructor-foundedinstructorsbyfirstname";
+//    }
+
     @RequestMapping(method = RequestMethod.GET, path = "/instructors/findByFirstName")
-    public String afindByFirstNamePostFormShowForm(){
+    public String findByFirstNamePostFormShowForm(){
         return "instructor-findbyfirstname";
     }
 }
--- src/main/web/pages/instructor-findbyfirstname.jsp
+++ src/main/web/pages/instructor-findbyfirstname.jsp
@@ -1,23 +1,17 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html>
 <head>
-    <title>Поиск инструктора по фамилии</title>
+    <title>Поиск инструктора</title>
 </head>
 <body>
-    <h1>Поиск тренера по фамилии</h1>
+    <h1>Поиск инструктора по фамилии</h1>
     <form action="/instructors/findByFirstName" method="post" enctype="application/x-www-form-urlencoded">
         <p><b>Введите фамилию инструктора:</b><br>
-            <input type="text" size="40">
+            <input type="text" size="40" name="firstName">
         </p>
         <p>
             <input type="submit" value="Найти"/>
         </p>
     </form>
-
-<!--    <p>
-        <a href="">Найти</a>
-    </p>
--->
-
 </body>
 </html>
